a231146403e826168e8633dc4267c0e3d2f70f7d
Rahul Srivastav <rsrahul7838@gmail.com>
2024-02-29 10:05:26 +0530

    netflix-commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      <h1> Hello Test</h1>
+    </div>
+  );
+}
+
+export default App;
